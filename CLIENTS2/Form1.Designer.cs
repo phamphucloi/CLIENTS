12e622cd014a5479617fea21ba03563a0581e464
Lợi Phạm Phúc <loi.pham@dptsolution.net>
2024-05-18 19:08:52 +0700

    Add project files.

---
+++ CLIENTS2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CLIENTS2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstChatBox = new System.Windows.Forms.ListBox();
+            this.txtMess = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtReceiver = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnJoinChat = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstChatBox
+            // 
+            this.lstChatBox.FormattingEnabled = true;
+            this.lstChatBox.ItemHeight = 16;
+            this.lstChatBox.Location = new System.Drawing.Point(126, 173);
+            this.lstChatBox.Name = "lstChatBox";
+            this.lstChatBox.Size = new System.Drawing.Size(551, 260);
+            this.lstChatBox.TabIndex = 17;
+            // 
+            // txtMess
+            // 
+            this.txtMess.Location = new System.Drawing.Point(205, 122);
+            this.txtMess.Multiline = true;
+            this.txtMess.Name = "txtMess";
+            this.txtMess.Size = new System.Drawing.Size(123, 32);
+            this.txtMess.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Message";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(389, 117);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 32);
+            this.btnSend.TabIndex = 14;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtReceiver
+            // 
+            this.txtReceiver.Location = new System.Drawing.Point(205, 75);
+            this.txtReceiver.Multiline = true;
+            this.txtReceiver.Name = "txtReceiver";
+            this.txtReceiver.Size = new System.Drawing.Size(123, 32);
+            this.txtReceiver.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Chat To :";
+            // 
+            // btnJoinChat
+            // 
+            this.btnJoinChat.Location = new System.Drawing.Point(389, 17);
+            this.btnJoinChat.Name = "btnJoinChat";
+            this.btnJoinChat.Size = new System.Drawing.Size(75, 32);
+            this.btnJoinChat.TabIndex = 11;
+            this.btnJoinChat.Text = "Join Chat";
+            this.btnJoinChat.UseVisualStyleBackColor = true;
+            this.btnJoinChat.Click += new System.EventHandler(this.btnJoinChat_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(205, 17);
+            this.txtUserName.Multiline = true;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(123, 32);
+            this.txtUserName.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "User Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstChatBox);
+            this.Controls.Add(this.txtMess);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtReceiver);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnJoinChat);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstChatBox;
+        private System.Windows.Forms.TextBox txtMess;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtReceiver;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnJoinChat;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CLIENTS2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLIENTS2
+{
+    public partial class Form1 : Form
+    {
+
+        HubConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new HubConnectionBuilder().WithUrl("http://localhost:5067/chatHub").Build();
+            connection.StartAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (connection != null)
+            {
+                connection.On<string, string>("ReceiveMessage",
+                    (string user, string message) =>
+                    {
+                        DateTime getDate = DateTime.Now;
+                        if (lstChatBox.InvokeRequired)
+                        {
+                            lstChatBox.Invoke((MethodInvoker)delegate ()
+                            {
+                                lstChatBox.Items.Add(message + "\t\t" + getDate.ToString("yyyy/MM/dd - HH:mm:ss"));
+                            }
+                            );
+                        }
+                    });
+            }
+        }
+
+        private void btnJoinChat_Click(object sender, EventArgs e)
+        {
+            UserConnection userConnection = new UserConnection();
+            userConnection.ConnectionID = connection.ConnectionId;
+            userConnection.UserName = txtUserName.Text;
+            connection.InvokeCoreAsync("JoinChat", new object[] { userConnection });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            connection.InvokeCoreAsync("SendMessage", new object[] { txtReceiver.Text, txtUserName.Text, txtUserName.Text + ": " + txtMess.Text });
+        }
+    }
+
+    public class UserConnection
+    {
+        public string UserName { get; set; }
+        public string ConnectionID { get; set; }
+    }
+}
